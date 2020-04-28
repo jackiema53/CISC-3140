e4468c5ec825c1c3ccd0c05bda078c8e8881104f
jackiema53 <60711158+jackiema53@users.noreply.github.com>
2020-04-28 16:03:03 -0400

    Update script.js

--- midterm pt2/script.js
+++ midterm pt2/script.js
@@ -3,7 +3,7 @@
 function myKeyPress(e){
 
   /* TODO: retrieve the value from the text input */
-  var mytextbox = "my textbox element";
+ var myphone = document.getElementById("phone").value;
 
 
   // TODO: set the value of the textbox with the formatted value
